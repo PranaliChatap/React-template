1a9d97b85726be0411a45b1f701bc37f0a526504
Pranali <pranalichatap010@gmail.com>
2022-11-09 17:03:43 +0530

    template

---
+++ template/src/components/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../css/style.css'
+
+ const Home = () => {
+  return (
+    <div className='home'>
+     
+    <h1 className='head1'>HTML Website Templates Free Download</h1>
+    <p className='para1'>A large program was prepared this year for the children; master classes, games, children's art fair.</p>
+    <button type='button' className='btn btn-danger '>Buy ticket</button>
+    
+     <div className="container">
+     <div class="row">
+  <div className='col-md border m-3 border-secondary'>hello</div>
+  <div className='col-md border m-3 border-secondary'>hello</div>
+  <div className='col-md border m-3 border-secondary'>hello</div>
+  <div className='col-md border m-3 border-secondary'>hello</div>
+  </div>
+     </div>
+    </div>
+  )
+}
+
+export default Home
